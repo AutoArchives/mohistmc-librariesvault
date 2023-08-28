d1470d015dfd39dee9f2b412b3574f3f1e64ca9b
Mgazul <mgazul@163.com>
2023-08-28 09:45:25 +0800

    init

---
+++ src/main/java/com/mohistmc/librariesvault/Main.java
@@ -0,0 +1,11 @@
+package com.mohistmc.librariesvault;
+
+/**
+ * @author Mgazul by MohistMC
+ * @date 2023/8/28 8:49:29
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
